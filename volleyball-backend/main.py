ce5e936f67daa7e8f1eba1789039ea56f742dd2d
Razvan Tonca <05razvan@gmail.com>
2026-08-17 23:34:44 +0100

    add vercel and railway URL to CORS

--- volleyball-backend/main.py
+++ volleyball-backend/main.py
@@ -29,7 +29,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://volleyball-analytics-tool-7wdj.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
