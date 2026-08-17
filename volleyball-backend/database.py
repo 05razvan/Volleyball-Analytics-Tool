ee96e6486756283224ec337cd0a1e32a342ef7d7
Razvan Tonca <05razvan@gmail.com>
2026-08-17 13:09:49 +0100

    Add deployment config

--- volleyball-backend/database.py
+++ volleyball-backend/database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:Password!@localhost/volleyball_db"
+)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
