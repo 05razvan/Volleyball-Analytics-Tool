6dd764644d8d2688c82068ad3af4917dedf778ba
Razvan Tonca <05razvan@gmail.com>
2026-08-18 11:40:56 +0100

    Fix URL

--- volleyball-frontend/src/pages/TeamDetail.js
+++ volleyball-frontend/src/pages/TeamDetail.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getPlayerAnalytics } from '../api';
 
-const api_base = 'volleyball-analytics-tool-production.up.railway.app';
+const api_base = 'https://volleyball-analytics-tool-production.up.railway.app';
 
 function TeamDetail() {
   const { teamId } = useParams();
